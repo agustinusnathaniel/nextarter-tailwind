b3447460a948c149262b2eb19eaccc1a1b3daed6
Agustinus Nathaniel <berserk.nate@gmail.com>
2025-05-04 10:52:36 +0700

    docs: update README

--- knip.ts
+++ knip.ts
@@ -3,7 +3,7 @@ import type { KnipConfig } from 'knip';
 const config: KnipConfig = {
   entry: ['src/index.tsx'],
   project: ['src/**/*.{ts,tsx,js,jsx,css,scss}'],
-  ignoreDependencies: ['next-seo', 'eslint-config-next'],
+  ignoreDependencies: ['eslint-config-next'],
   ignore: ['.eslintrc.*'],
   ignoreBinaries: ['changelogithub'],
 };
